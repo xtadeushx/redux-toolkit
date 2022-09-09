e403a49078f747154ba8764d261e835e8fecc4ac
xtadeushx <alekscherkasov13@gmail.com>
2022-09-09 14:23:16 +0300

    init: init app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = (title) => {
+    if (text.trim().length) {
+      setTodos((prev) => {
+        return [
+          ...prev,
+          {
+            id: new Date().toISOString(),
+            title,
+            completed: false,
+          },
+        ];
+      });
+      setText('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="">
+        <input
+          type="text"
+          placeholder="add todo"
+          name="todos"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={() => addTodo(text)}>Add Todo</button>
+      </label>
+      <ul>
+        {todos.map((el) => (
+          <li key={el.id}>
+            <input type="checkbox" checked={el.completed} />
+            <span>{el.title}</span>
+            <span className='delete'>&times;</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
